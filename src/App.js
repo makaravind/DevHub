914d2a80ea0cf306e326492274393614b51ba388
Nitin Patel <nitinpatel278@gmail.com>
2018-05-16 21:24:24 +0530

    Nitin's first commmit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function DisplaySocialDetails({socialDetails}) {
+
+    return (
+        <div>
+            <p><strong>{socialDetails.provider}</strong>
+                {socialDetails.username !== undefined && " - " + socialDetails.username}
+                {socialDetails.channel !== undefined && " - " + socialDetails.channel}
+                {socialDetails.link !== undefined && " - " + socialDetails.link}
+                {socialDetails.url !== undefined && " - " + socialDetails.url}
+                {socialDetails.type !== undefined && " - " + socialDetails.type}
+                {" - " + socialDetails.engagement.join(", ")}
+            </p>
+        </div>
+    )
+}
+
+function Me({userDetails}) {
+
+    let allSocialDetails = [];
+
+    for(let userDetail of userDetails){
+        allSocialDetails.push(<DisplaySocialDetails socialDetails={userDetail}/>);
+    }
+    return <div><h1>Social Presence</h1> {allSocialDetails}</div>;
+}
+
+
+function App ({user}) {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={user.image} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome {user.firstName} {user.lastName}</h1>
         </header>
-        <p className="App-intro">
-          Journey of React begins....
-        </p>
+
+          {/*<DisplaySocialDetails socialDetails={user.details[0]} />*/}
+
+          <Me userDetails={user.details}/>
       </div>
     );
-  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import user from './user.json';
+ReactDOM.render(<App user={user}/>, document.getElementById('root'));
 registerServiceWorker();
