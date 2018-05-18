f684e932d490b37624b98149463930ca13407871
AravindM <aravindkumar.metku@github.com>
2018-05-18 22:40:30 +0530

    1. Changed SocialDetails to SocialDetail.js 2. Added Input box with event handler

--- src/Me.js
+++ src/Me.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import SocialDetails from "./SocialDetails";
+import React, {Component} from 'react';
+import SocialDetail from "./SocialDetail";
 
-function Me({userDetails}) {
-    return (
-        <div>
-            <h1>Social Presence</h1>
-            <ul>
-                {userDetails.map(detail => <li key={detail.id}><SocialDetails socialDetails={detail}/></li>)}
-            </ul>
-        </div>
-    )
+class Me extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filter: 'test'
+        }
+    }
+
+    handleFilterChange = (event) => {
+        let value = event.target.value;
+        this.setState(currentState => {
+           return {filter: value}
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Social Presence</h1>
+                <input type="text" value={this.state.filter} onChange={(e) => this.handleFilterChange(e)}/>
+                <ul>
+                    {this.props.userDetails.map(detail =>
+                        <li key={detail.id}>
+                            <SocialDetail socialDetail={detail}/>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Me
--- src/index.js
+++ src/index.js
@@ -3,45 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-window.data = {
-    firstName: 'Aravind',
-    lastName: 'M',
-    image: '',
-    details: [
-        {
-            id:1,
-            provider: 'facebook',
-            username: 'mak11195',
-            engagement: ['personal'],
-            link: ''
-        },
-        {
-            id:2,
-            provider: 'github',
-            username: 'makaravind',
-            engagement: ['professional'],
-            link: 'https://github.com/makaravind'
-        },
-        {
-            id:3,
-            provider: 'gmail',
-            username: 'aravindmetku@gmail.com',
-            engagement: ['professional', 'personal'],
-        },
-        {
-            id:4,
-            provider: 'youtube',
-            channel: 'amyourmove',
-            url: 'youtube.com/c/amyourmove',
-            engagement: ['professional'],
-        },
-        {
-            id:5,
-            provider: 'wordpress',
-            type: 'blog',
-            url: 'https://maravindblog.wordpress.com',
-            engagement: ['professional'],
-        }
-    ]
-};
-ReactDOM.render(<App data={window.data}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
