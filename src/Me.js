7022e7f4dd600c8f65e2553caf76624a2ebbccfb
Rohan Paul <rohanpaul2@gmail.com>
2018-05-19 17:40:49 +0530

    implementing search by typing text in the input box

--- src/Me.js
+++ src/Me.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import SocialDetail from "./SocialDetail";
+import SocialDetails from "./SocialDetails";
+import './App.css';
 
 class Me extends Component {
 
@@ -7,32 +8,43 @@ class Me extends Component {
         super(props);
 
         this.state = {
-            filter: 'test'
+            filter: 'text',
+            filteredProfiles: []
         }
     }
 
-    handleFilterChange = (event) => {
-        let value = event.target.value;
-        this.setState(currentState => {
-           return {filter: value}
+    handleFilterChange = (e) => {
+        var value = e.target.value;
+        this.setState((currentState) => {
+            return {filter: value}
+        })
+    };
+
+    changeFilteredProfiles = () => {
+        let filteredProfiles = this.props.userDetails.filter((profile) => {
+            if (profile.engagement.indexOf(this.state.filter) !== -1) {
+                return this.props.userDetails;
+            }
         });
+
+        this.setState((currentState) => {
+            return {filteredProfiles: filteredProfiles}
+        })
+
     };
 
     render() {
+
         return (
             <div>
                 <h1>Social Presence</h1>
-                <input type="text" value={this.state.filter} onChange={(e) => this.handleFilterChange(e)}/>
+                <input name="filter" placeholder={this.props.placeholder} type="text" value={this.state.filter} onChange={(event) => this.handleFilterChange(event)}/>
+                <button class="button" onClick={this.changeFilteredProfiles}>Search</button>
                 <ul>
-                    {this.props.userDetails.map(detail =>
-                        <li key={detail.id}>
-                            <SocialDetail socialDetail={detail}/>
-                        </li>
-                    )}
+                    {this.state.filteredProfiles.map((detail) => <li key={detail.id}><SocialDetails socialDetails={detail}/></li>)}
                 </ul>
             </div>
-        )
-    }
+        )}
 }
 
-export default Me
+export default Me;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.data = {
+    firstName: 'Aravind',
+    lastName: 'M',
+    image: '',
+    details: [
+        {
+            id:1,
+            provider: 'facebook',
+            username: 'mak11195',
+            engagement: ['personal'],
+            link: ''
+        },
+        {
+            id:2,
+            provider: 'github',
+            username: 'makaravind',
+            engagement: ['professional'],
+            link: 'https://github.com/makaravind'
+        },
+        {
+            id:3,
+            provider: 'gmail',
+            username: 'aravindmetku@gmail.com',
+            engagement: ['professional', 'personal'],
+        },
+        {
+            id:4,
+            provider: 'youtube',
+            channel: 'amyourmove',
+            url: 'youtube.com/c/amyourmove',
+            engagement: ['professional'],
+        },
+        {
+            id:5,
+            provider: 'wordpress',
+            type: 'blog',
+            url: 'https://maravindblog.wordpress.com',
+            engagement: ['professional'],
+        }
+    ]
+};
+ReactDOM.render(<App data={window.data}/>, document.getElementById('root'));
